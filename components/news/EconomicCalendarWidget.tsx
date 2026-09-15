2096a8b6d8fc9328b751e364611beca361a45d21
Loki4x <muhamadrivalfx@gmail.com>
2026-09-16 03:57:48 +0700

    Update EconomicCalendarWidget.tsx

--- components/news/EconomicCalendarWidget.tsx
+++ components/news/EconomicCalendarWidget.tsx
@@ -1,25 +1,34 @@
 "use client";
 
+import { useEffect, useRef } from "react";
+
 export function EconomicCalendarWidget() {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+    containerRef.current.innerHTML = "";
+
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.async = false;
+    script.text = JSON.stringify({
+      colorTheme: "dark",
+      isTransparent: true,
+      width: "100%",
+      height: "650",
+      locale: "en",
+      importanceFilter: "-1,0,1",
+      countryFilter: "",
+    });
+    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-events.js";
+
+    containerRef.current.appendChild(script);
+  }, []);
+
   return (
-    <div className="tradingview-widget-container">
+    <div className="tradingview-widget-container" ref={containerRef}>
       <div className="tradingview-widget-container__widget" />
-      <script
-        type="text/javascript"
-        src="https://s3.tradingview.com/external-embedding/embed-widget-events.js"
-        async
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            colorTheme: "dark",
-            isTransparent: true,
-            width: "100%",
-            height: "650",
-            locale: "en",
-            importanceFilter: "-1,0,1",
-            countryFilter: "",
-          }),
-        }}
-      />
     </div>
   );
 }
