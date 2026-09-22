47c6f3392407f313c054ef2acaafb6dca42a0e5e
Loki4x <muhamadrivalfx@gmail.com>
2026-09-22 11:19:10 +0700

    Update Hero.tsx

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -66,7 +66,7 @@ export function Hero() {
                     <polyline
                       points="0,70 40,62 80,66 120,45 160,50 200,28 240,34 300,10"
                       fill="none"
-                      stroke="#118EEA"
+                      stroke="#5F85DB"
                       strokeWidth="2.5"
                     />
                   </svg>
